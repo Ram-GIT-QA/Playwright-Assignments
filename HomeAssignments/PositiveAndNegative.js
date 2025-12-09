2968db0e13c64b2ac9feba99982531a53c14e3e0
Ram-GIT-QA <124114743+Ram-GIT-QA@users.noreply.github.com>
2025-12-09 19:24:25 +0530

    Add files via upload

---
+++ HomeAssignments/PositiveAndNegative.js
@@ -0,0 +1,13 @@
+let i=0
+if(i>0)
+{
+    console.log("The Assigned Number is POSITIVE")
+}
+else if(i<0)
+{
+    console.log("The Assigned Number is NEGATIVE")
+}
+else
+{
+    console.log("The Assigned Number is NEUTRAL")
+}
---
+++ HomeAssignments/PrimitiveDatatypes.js
@@ -0,0 +1,12 @@
+let firstName = 'Ram'
+let companyName = "Goolge Inc.,"
+let mobileNumber = 1234567890
+let isAutomation = true
+let hasPlaywright
+
+console.log("The First Name of the Person is: "+firstName+"\t And its datatype is: "+typeof firstName)
+console.log("The Company Name is: "+companyName+"\t And its datatype is :"+typeof companyName)
+console.log("The Mobile Number is: "+mobileNumber+"\t And its datatype is :"+typeof mobileNumber)
+console.log("Whether its Automate or not : "+isAutomation+"\t And its datatype is :"+typeof isAutomation)
+console.log("Checking the hasPlaywright datatype :"+typeof hasPlaywright)
+

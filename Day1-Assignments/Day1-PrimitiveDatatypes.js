4a0e4455538caf204f3f255102bc23669be5ca4a
Ram-GIT-QA <124114743+Ram-GIT-QA@users.noreply.github.com>
2025-12-11 08:28:30 +0530

    Add files via upload

---
+++ Day1-Assignments/Day1-PrimitiveDatatypes.js
@@ -0,0 +1,12 @@
+let firstName = 'Playwright'
+let companyName = "Microsoft"
+let mobileNumber = 9876543210
+let isAutomated = false
+let hasPlaywright 
+
+console.log("The Assigned First Name value = "+firstName+".\t Its Datatype ="+typeof firstName)
+console.log("The Assigned Company Name value = "+companyName+".\t Its Datatype ="+typeof companyName)
+console.log("The Assignment Mobile Number value = "+mobileNumber+". Its Datatype ="+typeof mobileNumber)
+console.log("The Assigned isAutomated value = "+isAutomated+". \t Its Datatype = "+typeof isAutomated)
+console.log("The unassigned hasPlaywright value = "+hasPlaywright+".  Its Datatype = "+typeof hasPlaywright)
+
